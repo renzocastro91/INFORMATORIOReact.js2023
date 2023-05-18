72e33a1b4ab917904e15580acf2e0e9903ae106a
Renzo <renzogustavocastro99@gmail.com>
2023-05-18 20:14:10 -0300

    Calculadora Terminada

--- Programas/calculadora/index.js
+++ Programas/calculadora/index.js
@@ -1,84 +1,123 @@
-console.log(eval('8+9'))
-// Crear elementos HTML para la calculadora
-const calculator = document.createElement('div');
-calculator.classList.add('calculator');
+// Creo los elementos HTML para la calculadora
+const calculadora = document.createElement('div');
+calculadora.classList.add('calculadora', 'container');
 
-const output = document.createElement('div');
-output.classList.add('output');
-output.textContent = '0';
-calculator.appendChild(output);
+const salida = document.createElement('div');
+salida.classList.add('output');
+salida.textContent = '0';
+calculadora.appendChild(salida);
 
-const buttons = [
-  'C', 'CE', '%', '/',
-  '7', '8', '9', '*',
-  '4', '5', '6', '-',
-  '1', '2', '3', '+',
-  '0', '.', '='
-];
-
-buttons.forEach(button => {
+//Creo los botones
+const botones = ['C', 'CE', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=', '!'];
+//Inserto botones a calculadora y defino las clases para los diferentes botones
+botones.forEach(button => {
   const btn = document.createElement('button');
   btn.textContent = button;
-  btn.addEventListener('click', handleClick);
-  calculator.appendChild(btn);
+  btn.addEventListener('click', hacerClick);
+  if (button === '=' || button === 'C' || button === 'CE') {
+    btn.classList.add('specialButton');
+  }else if (button === "%" || button === "/" || button === "*" || button === "-" || button === "+" || button === '!'){
+    btn.classList.add('operator')
+  }
+  calculadora.appendChild(btn);
 });
 
-// Agregar la calculadora al elemento con id "app"
-const app = document.getElementById('app')
-app.appendChild(calculator);
+// Agrego la calculadora a "app"
+const app = document.getElementById('app');
+app.appendChild(calculadora);
 
-// Función para manejar los eventos de clic en los botones
-function handleClick(event) {
+let operacionActual = '';
+
+// Función para manejar los eventos de click en los botones
+function hacerClick(event) {
   const value = event.target.textContent;
-  
+
   switch (value) {
     case '=':
-      calculate();
+      guardarResultado();
+      calcular();
       break;
     case 'C':
-      clearOutput();
+      limpiarSalida();
       break;
     case 'CE':
-      clearEntry();
+      limpiarEntrada();
+      break;
+    case '!':
+      factorial();
       break;
     default:
-      updateOutput(value);
+      actualizarSalida(value);
       break;
   }
 }
 
-// Función para actualizar la salida de la calculadora
-function updateOutput(value) {
-  const currentOutput = output.textContent;
-
-  if (currentOutput === '0') {
-    output.textContent = value;
-  } else {
-    output.textContent += value;
-  }
+//Con esta función guardo la salida de una operación para poder utilizarla si se quiere en una siguiente operación
+function guardarResultado() {
+  const resultado = salida.textContent;
+  salida.textContent = '0';
+  operacionActual = resultado;
 }
 
-// Función para realizar el cálculo utilizando la función eval() que recibe el string (currentOutput) 
-//que contiene la operación en forma de string por ejemplo "8+2", y luego con eval(currentOutput)
-//realizamos la operación, y luego por último actualizamos el output con el valor del resultado 
-function calculate() {
-  const currentOutput = output.textContent;
 
+//Función para resolver el factorial
+function factorial(){
   try {
-    const result = eval(currentOutput);
-    output.textContent = result;
+    const num = parseInt(salida.textContent);
+    let factorial = 1;
+
+    if (num < 0) {
+      salida.textContent = 'Error';
+      return;
+    }
+
+    for (let i = 2; i <= num; i++) {
+      factorial *= i;
+    }
+
+    salida.textContent = factorial;
   } catch (error) {
-    output.textContent = 'Error';
+    salida.textContent = 'Error';
   }
+
+  operacionActual = '';
+}
+
+
+// Con esto actualizamos la salida de la calculadora
+function actualizarSalida(value) {
+  const actualSalida = salida.textContent;
+
+  if (actualSalida === '0') {
+    salida.textContent = value;
+  } else {
+    salida.textContent += value;
+  }
+
+  operacionActual += value;
+}
+
+// Función para realizar el cálculo utilizando la función eval()
+function calcular() {
+  try {
+    const result = eval(operacionActual);
+    salida.textContent = result;
+  } catch (error) {
+    salida.textContent = 'Error';
+  }
+
+  operacionActual = '';
 }
 
 // Función para borrar la salida "C"
-function clearOutput() {
-  output.textContent = '0';
+function limpiarSalida() {
+  salida.textContent = '0';
+  operacionActual = '';
 }
 
 // Función para borrar el último carácter de la salida "CE"
-function clearEntry() {
-  const currentOutput = output.textContent;
-  output.textContent = currentOutput.slice(0, -1);
+function limpiarEntrada() {
+  const actualSalida = salida.textContent;
+  salida.textContent = actualSalida.slice(0, -1);
+  operacionActual = operacionActual.slice(0, -1);
 }
